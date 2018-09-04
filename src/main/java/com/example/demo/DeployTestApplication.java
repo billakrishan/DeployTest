6f2958a651e4fa797100dcc54add7db134d8a68b
Krishan <billak@sheridancollege.ca>
2018-09-04 14:51:41 -0400

    Changing packaging to WAR

--- src/main/java/com/example/demo/DeployTestApplication.java
+++ src/main/java/com/example/demo/DeployTestApplication.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.example" })
-public class DeployTestApplication {
+public class DeployTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeployTestApplication.class, args);
